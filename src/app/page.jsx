4fa19696e5c9d1d502ff85aa6c40e8860ff46d89
amareLD <80774058+amareLD@users.noreply.github.com>
2024-01-29 22:35:35 +0530

    fix-eslint-prettier-autosave-problem

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,14 @@
-import styles from "./homepage.module.css";
+import Navbar from '@/componnents/navbar/Navbar';
+import styles from './homepage.module.css';
+import Link from 'next/link';
+import Footer from '@/componnents/footer/Footer';
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Navbar />
+
+      <Footer />
+    </div>
+  );
 }
