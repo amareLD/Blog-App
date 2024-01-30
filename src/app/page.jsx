d0ac28a46b2ee582e87e35b852f9b41f3c1b643f
amareLD <80774058+amareLD@users.noreply.github.com>
2024-01-30 22:32:50 +0530

    make-components-folders

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,22 @@
-import Navbar from '@/componnents/navbar/Navbar';
-import styles from './homepage.module.css';
 import Link from 'next/link';
-import Footer from '@/componnents/footer/Footer';
+import styles from './homepage.module.css';
+import Featured from '@/componnents/featured/Featured';
+import CategoryList from '@/componnents/categoryList/CategoryList';
+import CardList from '@/componnents/cardList/CardList';
+import Menu from '@/componnents/Menu/Menu';
 
-export default function Home() {
+export default function Home({ searchParams }) {
+  // const page = parseInt(searchParams.page) || 1;
+
   return (
-    <div>
-      <Navbar />
-
-      <Footer />
+    <div className={styles.container}>
+      <Featured />
+      <CategoryList />
+      <div className={styles.content}>
+        <CardList />
+        <Menu />
+      </div>
     </div>
   );
 }
+                
