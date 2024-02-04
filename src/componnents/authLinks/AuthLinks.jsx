e7950bbac6e67fbf9ac01c34df5a471e5dde75e4
amareLD <80774058+amareLD@users.noreply.github.com>
2024-02-04 19:39:26 +0530

    make-dark-light-mode

--- src/componnents/authLinks/AuthLinks.jsx
+++ src/componnents/authLinks/AuthLinks.jsx
@@ -1,10 +1,30 @@
+// import React from 'react';
+// import styles from './authLinks.module.css';
+// import { Link } from 'next/link'; // Correct import with curly braces
+
+// const AuthLinks = () => {
+//   // temporary
+//   const status = 'notauthenticated';
+//   return (
+//     <>
+//       {status === 'notauthenticated' ? (
+//         <Link href="/login">Login</Link>
+//       ) : (
+//         <Link href="/write">Write</Link>
+//       )}
+//     </>
+//   );
+// };
+
+// export default AuthLinks;
+
 import React from 'react';
 import styles from './authLinks.module.css';
-import { Link } from 'next/link';
+import  Link  from 'next/link'; // Correct import with curly braces
 
 const AuthLinks = () => {
-  //temporary
-  const status = 'authenticated';
+  // temporary
+  const status = 'notauthenticated';
   return (
     <>
       {status === 'notauthenticated' ? (
