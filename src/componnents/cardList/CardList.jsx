e3acb62c7597af126ec76f1967858527a88e64cd
amareLD <lahirudarshana.007@gmail.com>
2024-05-15 14:30:42 +0530

    updates

--- src/componnents/cardList/CardList.jsx
+++ src/componnents/cardList/CardList.jsx
@@ -5,7 +5,13 @@ import Pagination from '../pagination/Pagination';
 const CardList = () => {
   return (
     <div className={styles.container}>
-      <Pagination />
+      <h1 className={styles.title}>Recent Posts</h1>
+      <div className={styles.posts}>
+        {/* {posts?.map((item) => (
+          <Card item={item} key={item._id} />
+        ))} */}
+      </div>
+      <Pagination page={page} hasPrev={hasPrev} hasNext={hasNext} />
     </div>
   );
 };
