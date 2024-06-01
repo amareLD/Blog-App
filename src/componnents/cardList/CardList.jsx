159987e03b9d7ca104093ebc903a45d317870555
amareLD <lahirudarshana.007@gmail.com>
2024-06-02 01:08:41 +0530

    update-connection-of-mongodb-make-prisma-scheme

--- src/componnents/cardList/CardList.jsx
+++ src/componnents/cardList/CardList.jsx
@@ -1,8 +1,32 @@
-import React from 'react';
-import styles from './cardList.module.css';
-import Pagination from '../pagination/Pagination';
+import React from "react";
+import styles from "./cardList.module.css";
+import Pagination from "../pagination/Pagination";
+import Image from "next/image";
+import Card from "../card/Card";
 
-const CardList = () => {
+const getData = async (page, cat) => {
+  const res = await fetch(
+    `http://localhost:3000/api/posts?page=${page}&cat=${cat || ""}`,
+    {
+      cache: "no-store",
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+
+  return res.json();
+};
+
+const CardList = async ({ page, cat }) => {
+  const { posts, count } = await getData(page, cat);
+
+  const POST_PER_PAGE = 2;
+
+  const hasPrev = POST_PER_PAGE * (page - 1) > 0;
+  const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count;
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Recent Posts</h1>
@@ -11,7 +35,7 @@ const CardList = () => {
           <Card item={item} key={item._id} />
         ))}
       </div>
-      {/* <Pagination page={page} hasPrev={hasPrev} hasNext={hasNext} /> */}
+      <Pagination page={page} hasPrev={hasPrev} hasNext={hasNext} />
     </div>
   );
 };
